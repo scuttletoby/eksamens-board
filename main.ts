05e108c8de902ded53688b0569f738fba632828c
scuttletoby <123843277+scuttletoby@users.noreply.github.com>
2024-04-20 08:20:45 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
+let stateVar = 0
+input.buttonA0.onEvent(ButtonEvent.Click, function () {
+    if (true) {
+        stateVar = 10
+        console.log(stateVar)
+        console.log("Hello World")
+    }
+})
 forever(function () {
 	
-})
-input.buttonA0.onEvent(ButtonEvent.Click, function() {
-    
 })
